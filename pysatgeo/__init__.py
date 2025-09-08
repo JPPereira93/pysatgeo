8daa68b6fd51cdabef394aac2f52586935a63664
João Pereira <90710707+JPPereira93@users.noreply.github.com>
2025-09-08 14:33:57 +0100

    Refactoring submodules

--- pysatgeo/__init__.py
+++ pysatgeo/__init__.py
@@ -4,5 +4,11 @@ __author__ = "Joao Pereira"
 __email__ = "joaopmpereira93@gmail.com"
 __version__ = "0.0.7"   # bump this whenever you release a new version
 
-# Expose functions from aux_functions.py at the top-level
-from .aux_functions import *
+
+from .raster import *
+from .vector import *
+from .download import *
+from .gee import *
+from .lidar import *
+from .netcdf import *
+from .common import *
