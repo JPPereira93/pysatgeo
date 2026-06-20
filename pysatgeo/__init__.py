91cd2b05308aefac67d8d6b7b4f7104711f69089
João Pereira <90710707+JPPereira93@users.noreply.github.com>
2026-06-20 13:46:00 +0100

    Bump version: 0.4.1 → 0.4.2

--- pysatgeo/__init__.py
+++ pysatgeo/__init__.py
@@ -2,5 +2,5 @@
 
 __author__ = "Joao Pereira"
 __email__ = "joaopmpereira93@gmail.com"
-__version__ = "0.4.1"   # bump this whenever you release a new version
+__version__ = "0.4.2"   # bump this whenever you release a new version
 
